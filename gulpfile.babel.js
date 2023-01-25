b203dd7e8045f0bbcc4d935e416183cc5d27150a
kalekseychuk@innovorg.com <kalekseychuk@innovorg.com>
2023-01-25 19:04:32 +0700

    feat: config - add new tasks in gulpfile

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,11 +1,10 @@
+import gulp from 'gulp';
 import config from './gulp/config';
+import clean from './gulp/tasks/clean';
+import server from './gulp/tasks/server';
 
 config.setEnv();
 
-export const build = () => {
-  console.log(config.isProd);
-};
+export const build = gulp.series(clean);
 
-export const watch = () => {
-  console.log(config.isProd);
-};
+export const watch = gulp.series(build, server);
