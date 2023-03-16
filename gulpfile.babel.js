b8f5e157a9dea5fa54a91628bdd3c022b5bd2c02
WhiteBearDeveloper <viper010790@mail.ru>
2023-03-16 23:06:22 +0700

    feat: tasks - added new tasks

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -2,9 +2,14 @@ import gulp from 'gulp';
 import config from './gulp/config';
 import clean from './gulp/tasks/clean';
 import server from './gulp/tasks/server';
+import { scriptsBuild, scriptsWatch } from './gulp/tasks/scripts';
 
 config.setEnv();
 
-export const build = gulp.series(clean);
+export const build = gulp.series(clean, scriptsBuild);
 
-export const watch = gulp.series(build, server);
+export const watch = gulp.series(
+  // build,
+  server,
+  scriptsWatch,
+);
